1c50a52fdd74c9eb819444fd6a8b5e5703a41d16
savegnago <savegdesigner@gmail.com>
2020-05-26 22:36:53 -0300

    create-obras component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http'
+import { ReactiveFormsModule } from '@angular/forms'
 
 // Components
 import { AppComponent } from './app.component';
@@ -8,8 +10,7 @@ import { HeaderComponent } from './template/header/header.component';
 import { HomeComponent } from './views/home/home.component';
 import { PerfilComponent } from './components/user/perfil/perfil.component';
 import { UserObrasComponent } from './components/user/user-obras/user-obras.component';
-import { HttpClientModule } from '@angular/common/http'
-import { ReactiveFormsModule } from '@angular/forms'
+
 
 // Bootstrap
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
@@ -17,8 +18,6 @@ import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { UserObraCreateComponent } from './components/user/user-obra-create/user-obra-create.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { SignupComponent } from './auth/signup/signup.component';
-
-
 
 @NgModule({
   declarations: [
--- frontend/src/app/components/user/user-obra-create/user-obra-create.component.ts
+++ frontend/src/app/components/user/user-obra-create/user-obra-create.component.ts
@@ -9,16 +9,40 @@ import Obra from 'src/app/models/Obra.model';
 })
 export class UserObraCreateComponent implements OnInit {
 
-  private obra: Obra
-  private userId: number 
+  public obra: Obra
+  public userId: string 
+  public imageStyle: object
+
+  public editorForm: FormGroup = new FormGroup({
+    grayscale: new FormControl(),
+    hue: new FormControl(),
+    saturate: new FormControl(),
+    sepia: new FormControl()
+  })
 
   constructor() { }
 
   ngOnInit(): void {
+    this.userId = localStorage.getItem('userId')
+    
   }
 
-  public create(): void {
+  public aplicar(): void {
+    let grayscale = this.editorForm.value.grayscale || 0
+    let hue = this.editorForm.value.hue || 0
+    let saturate = this.editorForm.value.saturate || 0
+    let sepia = this.editorForm.value.sepia || 0
 
+    this.imageStyle = {
+      'filter': `grayscale(${grayscale}%) hue-rotate(${hue}deg) saturate(${saturate}%) sepia(${sepia}%)`
+    }
+
+      console.log(this.imageStyle)
+
+  }
+
+  public getImageFile(event): void {
+    console.log(event.target.files)
   }
 
 }
